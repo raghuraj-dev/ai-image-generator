d7f1459ce580abcb9c517ee7d47362dfb39c6b43
raghuraj <balasubramaniam.raghuraj@gmail.com>
2023-04-11 15:14:49 +0530

    implementing the chat gpt prompt suggestion functionality

--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -1,11 +1,21 @@
-const { app } = require('@azure/functions');
+const { app } = require("@azure/functions");
+const openai = require("../../lib/openai");
 
-app.http('getChatGPTSuggestion', {
-    methods: ['GET'],
-    authLevel: 'anonymous',
+app.http("getChatGPTSuggestion", {
+    methods: ["GET"],
+    authLevel: "anonymous",
     handler: async (request, context) => {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: "Write a random text prompt for DALL•E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: Oil painting, watercolor, photo—realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
+            max_tokens: 100,
+            temperature: 0.8,
+        });
+
         context.log(`Http function processed request for url "${request.url}"`);
 
-        return { body: `Hello, world!` };
-    }
+        const responseText = response.data.choices[0].text;
+
+        return { body: responseText };
+    },
 });
--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -16,13 +16,19 @@ function PromptInput() {
         revalidateOnFocus: false,
     });
 
+    const loading = isLoading || isValidating;
+
     return (
         <div className="m-10">
             <form className="flex flex-col rounded-md border shadow-md shadow-slate-400/10 lg:flex-row lg:divide-x">
                 <textarea
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
-                    placeholder="Enter a prompt..."
+                    placeholder={
+                        (loading && "ChatGPT is thinking of a suggestion...") ||
+                        suggestion ||
+                        "Enter a prompt..."
+                    }
                     className="flex-1 rounded-md p-4 outline-none"
                 />
                 <button
@@ -45,10 +51,20 @@ function PromptInput() {
                 <button
                     type="button"
                     className="rounded-b-md border-none bg-white p-4 font-bold text-violet-400 transition-colors duration-200 md:rounded-r-md md:rounded-bl-none"
+                    onClick={mutate}
                 >
                     New Suggestion
                 </button>
             </form>
+
+            {input && (
+                <p className="pl-2 pt-2 font-light italic">
+                    Suggestion:{" "}
+                    <span className="text-violet-500">
+                        {loading ? "ChatGPT is thinking..." : suggestion}
+                    </span>
+                </p>
+            )}
         </div>
     );
 }

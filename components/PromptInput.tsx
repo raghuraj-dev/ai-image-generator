294dd2424f1ebfb2d95844e9cd4fec4f0e667dad
raghuraj <balasubramaniam.raghuraj@gmail.com>
2023-04-11 10:40:11 +0530

    implementing useSWR

--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import fetchSuggestionFromChatGPT from "@/lib/fetchSuggestionFromChatGPT";
 import { useState } from "react";
+import useSWR from "swr";
 
 function PromptInput() {
     const [input, setInput] = useState("");
+
+    const {
+        data: suggestion,
+        isLoading,
+        mutate,
+        isValidating,
+    } = useSWR("/api/suggestion", fetchSuggestionFromChatGPT, {
+        revalidateOnFocus: false,
+    });
 
     return (
         <div className="m-10">

9e1c87460459d29dba94ecceffc0e0605406b3d9
raghuraj <balasubramaniam.raghuraj@gmail.com>
2023-04-11 18:00:09 +0530

    Implementing the Generate Image & Use New suggesti

--- azure/src/functions/generateImage.js
+++ azure/src/functions/generateImage.js
@@ -51,5 +51,7 @@ app.http("generateImage", {
         } catch (error) {
             console.error("Error uploading file:", error.message);
         }
+
+        return { body: "Successfully uploaded Image" }
     },
 });
--- components/PromptInput.tsx
+++ components/PromptInput.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import fetchSuggestionFromChatGPT from "@/lib/fetchSuggestionFromChatGPT";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
 
 function PromptInput() {
@@ -16,11 +16,40 @@ function PromptInput() {
         revalidateOnFocus: false,
     });
 
+    const submitPrompt = async (useSuggestion?: boolean) => {
+        const inputPrompt = input;
+        setInput("");
+
+        console.log(inputPrompt);
+
+        // p is the prompt to send to API
+        const p = useSuggestion ? suggestion : inputPrompt;
+
+        const res = await fetch("/api/generateImage", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt: p }),
+        });
+
+        const data = await res.json();
+    };
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        await submitPrompt();
+    };
+
     const loading = isLoading || isValidating;
 
     return (
         <div className="m-10">
-            <form className="flex flex-col rounded-md border shadow-md shadow-slate-400/10 lg:flex-row lg:divide-x">
+            <form
+                onSubmit={handleSubmit}
+                className="flex flex-col rounded-md border shadow-md shadow-slate-400/10 lg:flex-row lg:divide-x"
+            >
                 <textarea
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
@@ -44,6 +73,7 @@ function PromptInput() {
                 </button>
                 <button
                     type="button"
+                    onClick={() => submitPrompt(true)}
                     className="bg-violet-400 p-4 font-bold text-white transition-colors duration-200 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-300"
                 >
                     Use Suggestion
